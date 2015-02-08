8f4b1c41e4712f3887aefbcd030ef3a8584b46ee
abgoyal <abgoyal@gmail.com>
2015-02-08 21:13:19 +0530

    Initial git commit from Alcatel Source repo @ http://sourceforge.net/projects/alcatel

---
+++ mediatek/custom/common/cgen/inc/audio_custom.h
@@ -0,0 +1,312 @@
+ 
+ 
+#ifndef AUDIO_CUSTOM_H
+#define AUDIO_CUSTOM_H
+ 
+/* define Gain For Normal */ 
+/* Normal volume: TON, SPK, MIC, FMR, SPH, SID, MED */ 
+ 
+#define GAIN_NOR_TON_VOL    8    // reserved 
+#define GAIN_NOR_KEY_VOL    43    // TTY_CTM_Mic 
+#define GAIN_NOR_MIC_VOL    26    // IN_CALL BuiltIn Mic gain 
+// GAIN_NOR_FMR_VOL is used as idle mode record volume 
+#define GAIN_NOR_FMR_VOL    0    // Normal BuiltIn Mic gain 
+#define GAIN_NOR_SPH_VOL    20    // IN_CALL EARPIECE Volume 
+#define GAIN_NOR_SID_VOL    100    // IN_CALL EARPICE sidetone 
+#define GAIN_NOR_MED_VOL    25    // reserved 
+ 
+/* define Gain For Headset */ 
+/* Headset volume: TON, SPK, MIC, FMR, SPH, SID, MED */ 
+ 
+#define GAIN_HED_TON_VOL    8    // reserved 
+#define GAIN_HED_KEY_VOL    24    // reserved 
+#define GAIN_HED_MIC_VOL    20    // IN_CALL BuiltIn headset gain 
+#define GAIN_HED_FMR_VOL    24    // reserved 
+#define GAIN_HED_SPH_VOL    12    // IN_CALL Headset volume 
+#define GAIN_HED_SID_VOL    100    // IN_CALL Headset sidetone 
+#define GAIN_HED_MED_VOL    12    // Idle, headset Audio Buf Gain setting 
+ 
+/* define Gain For Handfree */ 
+/* Handfree volume: TON, SPK, MIC, FMR, SPH, SID, MED */ 
+/* GAIN_HND_TON_VOL is used as class-D Amp gain*/ 
+ 
+#define GAIN_HND_TON_VOL    8    // use for ringtone volume 
+#define GAIN_HND_KEY_VOL    24    // reserved 
+#define GAIN_HND_MIC_VOL    20    // IN_CALL LoudSpeak Mic Gain = BuiltIn Gain 
+#define GAIN_HND_FMR_VOL    24    // reserved 
+#define GAIN_HND_SPH_VOL    12    // IN_CALL LoudSpeak 
+#define GAIN_HND_SID_VOL    100    // IN_CALL LoudSpeak sidetone 
+#define GAIN_HND_MED_VOL    12    // Idle, loudSPK Audio Buf Gain setting 
+ 
+    /* 0: Input FIR coefficients for 2G/3G Normal mode */
+    /* 1: Input FIR coefficients for 2G/3G/VoIP Headset mode */
+    /* 2: Input FIR coefficients for 2G/3G Handfree mode */
+    /* 3: Input FIR coefficients for 2G/3G/VoIP BT mode */
+    /* 4: Input FIR coefficients for VoIP Normal mode */
+    /* 5: Input FIR coefficients for VoIP Handfree mode */
+#define SPEECH_INPUT_FIR_COEFF \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0   
+    /* 0: Output FIR coefficients for 2G/3G Normal mode */
+    /* 1: Output FIR coefficients for 2G/3G/VoIP Headset mode */
+    /* 2: Output FIR coefficients for 2G/3G Handfree mode */
+    /* 3: Output FIR coefficients for 2G/3G/VoIP BT mode */
+    /* 4: Output FIR coefficients for VoIP Normal mode */
+    /* 5: Output FIR coefficients for VoIP Handfree mode */
+#define SPEECH_OUTPUT_FIR_COEFF \
+       31,  -459,   -15,  -612,  -230,\
+     -591,  -618,  -675,  -883,  -178,\
+    -1007,  -176, -1561,   624, -1943,\
+     1471, -1844,  2651, -6442,  6084,\
+    -4587, 23197, 23197, -4587,  6084,\
+    -6442,  2651, -1844,  1471, -1943,\
+      624, -1561,  -176, -1007,  -178,\
+     -883,  -675,  -618,  -591,  -230,\
+     -612,   -15,  -459,    31,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0   
+#define   DG_DL_Speech    0xE3D
+#define   DG_Microphone    0x1400
+#define   FM_Record_Vol    6     /* 0 is smallest. each step increase 1dB.
+                            Be careful of distortion when increase too much. 
+                            Generally, it's not suggested to tune this parameter */ 
+#define   DEFAULT_BLUETOOTH_SYNC_TYPE    0
+#define   DEFAULT_BLUETOOTH_SYNC_LENGTH    1
+    /* 0: Input FIR coefficients for 2G/3G Normal mode */
+    /* 1: Input FIR coefficients for 2G/3G/VoIP Headset mode */
+    /* 2: Input FIR coefficients for 2G/3G Handfree mode */
+    /* 3: Input FIR coefficients for 2G/3G/VoIP BT mode */
+    /* 4: Input FIR coefficients for VoIP Normal mode */
+    /* 5: Input FIR coefficients for VoIP Handfree mode */
+#define WB_Speech_Input_FIR_Coeff \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0   
+    /* 0: Output FIR coefficients for 2G/3G Normal mode */
+    /* 1: Output FIR coefficients for 2G/3G/VoIP Headset mode */
+    /* 2: Output FIR coefficients for 2G/3G Handfree mode */
+    /* 3: Output FIR coefficients for 2G/3G/VoIP BT mode */
+    /* 4: Output FIR coefficients for VoIP Normal mode */
+    /* 5: Output FIR coefficients for VoIP Handfree mode */
+#define WB_Speech_Output_FIR_Coeff \
+      206,  -204,   142,  -135,   133,   -22,  -204,   457,  -646,   735,\
+     -751,   622,  -446,   356,  -222,   -66,   418,  -667,   806, -1050,\
+     1369, -1645,  1880, -2001,  1866, -1400,   698,    85, -1197,  2949,\
+    -4916,  6086, -5947,  5057, -4236,  3131,  -695, -2753,  5618, -7848,\
+    11838,-19043, 26240,-28021, 32767, 32767,-28021, 26240,-19043, 11838,\
+    -7848,  5618, -2753,  -695,  3131, -4236,  5057, -5947,  6086, -4916,\
+     2949, -1197,    85,   698, -1400,  1866, -2001,  1880, -1645,  1369,\
+    -1050,   806,  -667,   418,   -66,  -222,   356,  -446,   622,  -751,\
+      735,  -646,   457,  -204,   -22,   133,  -135,   142,  -204,   206,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+                                      \
+    32767,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0,\
+        0,     0,     0,     0,     0,     0,     0,     0,     0,     0   
+#endif 
---
+++ mediatek/custom/jrdsh75_cu_jb/preloader/inc/custom_MemoryDevice.h
@@ -0,0 +1,12 @@
+
+#ifndef __CUSTOM_MEMORYDEVICE__
+#define __CUSTOM_MEMORYDEVICE__
+
+
+
+#define BOARD_ID                MT6575_EVB
+
+#define CS_PART_NUMBER[0]       H9TP32A4GDMCPR_KDM
+//#define CS_PART_NUMBER[1]       H9TP32A8JDMCPR_KGM
+
+#endif /* __CUSTOM_MEMORYDEVICE__ */
